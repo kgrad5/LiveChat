49c0e1df6c8754417e85f5c46bc841e8c262320f
Kevin Grad <kgrad5@gmail.com>
2012-06-11 22:25:33 -0400

    Added mongodb to make chat persistant

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 import os
+import pymongo
 
 from datetime import datetime
 from tornado.options import define, options
@@ -10,12 +11,13 @@ from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
-	chat = {}
 	
 	def __init__(self):
 		handlers = [
 			(r"/", IndexHandler),
 		]
+		conn = pymongo.Connection("localhost", 27017)
+		self.db = conn["chat"]
 		settings = dict(
 			template_path = os.path.join(os.path.dirname(__file__), "templates"),
 			static_path = os.path.join(os.path.dirname(__file__), "static"),
@@ -25,10 +27,17 @@ class Application(tornado.web.Application):
 		
 class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.render('index.html', chatlog = Application.chat)
+		coll = self.application.db.chat
+		self.render('index.html', chatlog = coll.find()) 
 	def post(self):
-		Application.chat[datetime.now().strftime('%x %X')] = self.get_argument('text') 
-		self.render('index.html', chatlog = Application.chat)
+		coll = self.application.db.chat
+		try:
+			text = self.get_argument('text')
+		except:
+			self.render('index.html', chatlog = coll.find())
+		
+		coll.insert({"message": text, "timestamp": datetime.now().strftime('%x %X')})
+		self.render('index.html', chatlog = coll.find())
 
 if __name__ == '__main__':
 	# Parse the command line options defined with define() statements, this comes from the
